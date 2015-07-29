cbaa01c5a85c8db5cdb388a1d341feb4d508dab7
Matt Fili <matthewjosephfili@gmail.com>
2015-07-29 11:00:42 -0500

    dev and test servers running with working integration tests

--- test/integration/routes.test.js
+++ test/integration/routes.test.js
@@ -4,6 +4,7 @@ var http = require('http');
 var path = require('path');
 
 describe('routes', function () {
+	this.timeout(30000)
 	var port = Math.floor(Math.random() * 30000 + 10000);
 	var url = 'http://localhost:' + port;
 
@@ -13,6 +14,13 @@ describe('routes', function () {
 
 	it('should respond to the /space route', function(done){
 		http.get(url +'/space', function(res){
+		    expect(res.statusCode).to.equal(200);
+		    done();
+		});
+	});
+
+	it('should respond to the /weather route', function(done){
+		http.get(url +'/weather', function(res){
 		    expect(res.statusCode).to.equal(200);
 		    done();
 		});
